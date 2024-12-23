f34e1af80f8f44ee7104d90a1625ff3fe118e8fe
Dikiy Perec <hopabsq@mail.ru>
2024-12-24 01:52:45 +0600

    fixed sizes

--- components/shared/profile/description.jsx
+++ components/shared/profile/description.jsx
@@ -3,7 +3,7 @@ export default function Description({ description }) {
         <div className="flex flex-col gap-2 w-full px-5">
             <div className="flex items-center">
                 <span className="text-2xl font-medium w-full">About me</span>
-                <button className="text-xl text-primary font-medium">Edit</button>
+                <button className="text-lg text-primary font-medium">Edit</button>
             </div>
             <p className="text-foreground/50 break-words text-lg">
                 {description?.length > 0  ? description : "No description."}
