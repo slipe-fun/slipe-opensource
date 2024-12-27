458aecabbc6987f42817b6abe86bef60707b16d7
Dikiy Perec <hopabsq@mail.ru>
2024-12-27 17:31:49 +0600

    fixed post sizes

--- components/shared/home/slides/post/post.jsx
+++ components/shared/home/slides/post/post.jsx
@@ -9,7 +9,7 @@ const Post = forwardRef(({ user, setUser, post, isPostDeleted, className}, ref) 
 	return (
 		<div
 		ref={ref}
-			className={cn('flex bg-[#161616] w-[calc(200%-2.5rem)] relative rounded-[2rem] justify-between h-full overflow-hidden items-center flex-col', className)}
+			className={cn('flex bg-[#161616] w-[calc(200%-2.5rem)] absolute rounded-[2rem] justify-between h-full overflow-hidden items-center flex-col', className)}
 		>
 			<UserBlock user={user} setUser={setUser} date={post?.date} />
 			<img loading='lazy' src={cdn + `/posts/${post?.image}`} className='w-full object-contain h-full absolute top-0 block' />
