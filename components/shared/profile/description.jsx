455052a18bf8feb9af5b65005d05e90bdebea994
pip64 <76626915+pip64@users.noreply.github.com>
2024-12-30 14:32:33 +0700

    Removed duplicate import and added passing of changeUserDescription to the component

--- components/shared/profile/description.jsx
+++ components/shared/profile/description.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import DescriptionModal from "../modals/description/description";
-import { useState } from "react";
 
 export default function Description({ user, changeUserDescription }) {
 	const [isEdit, setIsEdit] = useState(false);
@@ -9,7 +8,7 @@ export default function Description({ user, changeUserDescription }) {
 		<div className='flex flex-col gap-2 w-full px-5'>
 			<div className='flex items-center'>
 				<span className='text-2xl font-medium w-full'>About me</span>
-				<DescriptionModal open={isEdit} setOpen={setIsEdit}>
+				<DescriptionModal open={isEdit} setOpen={setIsEdit} user={user} changeUserDescription={changeUserDescription}>
 					<button className='text-lg text-primary font-medium'>Edit</button>
 				</DescriptionModal>
 			</div>
