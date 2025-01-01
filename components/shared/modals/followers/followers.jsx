a7a0ed4ecaf48b5a7d14503384e16302d8ff9312
Dikiy Perec <hopabsq@mail.ru>
2025-01-01 22:51:22 +0600

    Added user component to followers modal

--- components/shared/modals/followers/followers.jsx
+++ components/shared/modals/followers/followers.jsx
@@ -1,6 +1,7 @@
 import { Drawer, DrawerContent, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
 import SearchBar from "../search-modal/search-bar";
 import { useState } from "react";
+import User from "./user";
 
 //TODO: add user block and search(needs search page)
 
@@ -19,7 +20,7 @@ export default function FollowersModal({ children, user, open, setOpen }) {
 					<DrawerTitle className='font-medium'>{user.username}'s followers</DrawerTitle>
 				</DrawerHeader>
 				<ul data-shadowed={focus} className="w-full duration-200 !h-[31.5rem] overflow-y-auto ease-out data-[shadowed=true]:opacity-40 px-5 relative pb-[5.5rem] flex flex-col gap-5">
-
+					<User user={user}/>
 				</ul>
 				<DrawerFooter id='categories-scroller' className='p-5 w-full flex-row fixed items-end bottom-0 bg-modal z-10 flex gap-4'>
 					<SearchBar setFocus={setFocus} value={searchValue} setValue={setSearchValue} className="bg-foreground/[0.08]" placeholder="Search for followers"/>
--- components/shared/modals/followers/user.jsx
+++ components/shared/modals/followers/user.jsx
@@ -1,1 +1,52 @@
-//TODO: Complete this yo
+import { useEffect, useState } from "react";
+import { useStorage } from "@/hooks/contexts/session";
+import { Button } from "@/components/ui/button";
+import cdn from "@/constants/cdn";
+import PixelAvatar from "@/components/shared/pixels-avatar";
+import follow from "@/lib/users/follow";
+
+export default function User({ user }) {
+	const [localUser, setLocalUser] = useState(user);
+	const [state, setState] = useState(localUser?.subscribed);
+	const { token, store } = useStorage();
+
+	async function subscribe() {
+		if (state) setState(false);
+		else setState(true);
+
+		const followRequest = await follow(user?.id, token);
+
+		if (followRequest?.error) {
+			if (state) setState(true);
+			else setState(false);
+		}
+	}
+
+	useEffect(() => {
+		setLocalUser(user);
+		setState(user?.subscribed);
+	}, [user]);
+
+	return (
+		<div className='w-full flex gap-3 duration-200 ease-out active:opacity-80'>
+			<div className='w-full flex gap-3 items-center overflow-hidden'>
+				{localUser?.avatar ? (
+					<img loading='lazy' className='rounded-full w-12 h-12' src={`${cdn}/avatars/${localUser?.avatar}`} />
+				) : (
+					<PixelAvatar size={48} username={localUser?.username} pixels={localUser?.pixel_order} />
+				)}
+				<div className='flex flex-col w-full overflow-hidden'>
+					<div className='w-full flex gap-1'>
+						<div className='whitespace-nowrap overflow-hidden max-w-fit text-ellipsis font-medium text-white'>
+							{localUser?.nickname ? localUser?.nickname : localUser?.username}
+						</div>
+					</div>
+					<span className='text-sm text-white/50'>{user.username}</span>
+				</div>
+			</div>
+			<Button data-subscribed={state} className='data-[subscribed=true]:!bg-[#1F1F1F] rounded-full' onClick={subscribe}>
+				{state ? "Unfollow" : "Follow"}
+			</Button>
+		</div>
+	);
+}
