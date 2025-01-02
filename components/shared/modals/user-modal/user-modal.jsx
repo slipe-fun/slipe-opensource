406ba42a53780b0afd8a589e1f35cf9b4f13fa35
Dikiy Perec <hopabsq@mail.ru>
2025-01-02 21:58:50 +0600

    Fixed banner animation in user modal

--- components/shared/modals/user-modal/user-modal.jsx
+++ components/shared/modals/user-modal/user-modal.jsx
@@ -5,15 +5,15 @@ import { useEffect, useRef } from "react";
 
 export default function UserModal({ open, setOpen, user }) {
 	const profileModalRef = useRef(null);
-	const { scrollY } = useScroll({ container: profileModalRef, offset: ["40%", "-0%"] });
+	const { scrollY } = useScroll({ container: open ? profileModalRef : null, offset: ["40%", "-0%"] });
 
 	useEffect(() => {
-        console.log(1)
-    }, [scrollY])
+		console.log(profileModalRef, scrollY);
+	}, [scrollY, profileModalRef]);
 
 	return (
 		<PageModal className='bg-background' open={open}>
-			<div id='profileScrollModal' ref={profileModalRef} className='overflow-y-auto duration-300 ease-out flex flex-col w-full h-full'>
+			<div id='profileScrollModal' ref={open ? profileModalRef : null} className='overflow-y-auto duration-300 ease-out flex flex-col w-full h-full'>
 				<Banner user={user} scrollProgress={scrollY} />
 				<div className='w-full min-h-full flex flex-col gap-5'>
 					<User user={user} scrollProgress={scrollY} isModal />
--- components/shared/profile/banner.jsx
+++ components/shared/profile/banner.jsx
@@ -11,18 +11,20 @@ export default function Banner({ user = {}, scrollProgress }) {
 	const height = useTransform(scrollProgress, [0, 180], [bannerSize, 88]);
 
 	useEffect(() => {
-		setBannerSize(bannerRef.current?.getBoundingClientRect()?.height)
-	}, [])
+		setTimeout(() => {
+			setBannerSize(bannerRef.current?.getBoundingClientRect()?.height);
+		}, 300);
+	}, []);
 
 	return (
 		<>
 			<div
 				ref={bannerRef}
-				style={{ "--top-sticking": `${bannerSize - 88}px` }}
+				style={{ "--top-sticking": `${bannerSize <= 0 ? bannerSize : bannerSize - 88}px` }}
 				className='sticky -top-[--top-sticking] w-full overflow-hidden aspect-[16/11] min-h-fit z-30 rounded-b-[1.25rem]'
 			>
 				{user?.banner ? (
-					<img loading='lazy' src={cdn + "/banners/" + user?.banner} className='w-full absolute -z-10 h-full object-cover'/>
+					<img loading='lazy' src={cdn + "/banners/" + user?.banner} className='w-full absolute -z-10 h-full object-cover' />
 				) : (
 					<div className='grid grid-cols-7 bg-black grid-rows-1 absolute -z-10 h-full w-full'>
 						{ShufflePixels(user?.pixel_order)?.map((pixel, index) => (
@@ -42,7 +44,9 @@ export default function Banner({ user = {}, scrollProgress }) {
 					style={{ opacity, height }}
 					className='bottom-0 w-full flex items-center p-5 justify-center pointer-events-none text-white flex-col absolute bg-black/50'
 				>
-					<span className='font-medium text-lg overflow-hidden w-full whitespace-nowrap max-w-fit text-ellipsis text-center'>{user?.nickname ? user?.nickname : user?.username}</span>
+					<span className='font-medium text-lg overflow-hidden w-full whitespace-nowrap max-w-fit text-ellipsis text-center'>
+						{user?.nickname ? user?.nickname : user?.username}
+					</span>
 					<span className='opacity-50 overflow-hidden w-full whitespace-nowrap max-w-fit text-ellipsis text-center'>@{user?.username}</span>
 				</motion.div>
 			</div>
