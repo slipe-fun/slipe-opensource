c8b13f65d2952d1b76d7c38751f7cd50e27f63d7
pip64 <76626915+pip64@users.noreply.github.com>
2025-02-16 22:32:05 +0700

    Subscribe notifications slide in notifications modal

--- components/shared/modals/notifications/blocks/follow.jsx
+++ components/shared/modals/notifications/blocks/follow.jsx
@@ -2,22 +2,23 @@ import Img from "@/components/ui/image";
 import PixelAvatar from "@/components/shared/pixels-avatar";
 import cdn from "@/constants/cdn";
 import { Button } from "@/components/ui/button";
+import { TimePassedFromDate } from "@/lib/utils";
 
 export default function FollowBlock({ notification }) {
 	return (
 		<div className='w-full border border-foreground/[0.1] bg-block rounded-lg flex p-3 flex-col gap-[0.125rem]'>
 			<div className='w-full flex items-center gap-4'>
 				<div className='w-full flex gap-3 duration-200 ease-out items-center overflow-hidden active:opacity-80'>
-					{notification?.avatar ? (
-						<Img wrapperClassName='rounded-full min-w-11 h-11' iconClassName='!w-7 !h-7' src={`${cdn}/avatars/${notification?.avatar}`} />
+					{notification?.from_user?.avatar ? (
+						<Img wrapperClassName='rounded-full min-w-11 h-11' iconClassName='!w-7 !h-7' src={`${cdn}/avatars/${notification?.from_user?.avatar}`} />
 					) : (
-						<PixelAvatar size={44} username={notification?.username} pixels={notification?.pixel_order} />
+						<PixelAvatar size={44} username={notification?.from_user?.username} pixels={notification?.from_user?.pixel_order} />
 					)}
 					<div className='flex flex-col w-full overflow-hidden'>
 						<div className='w-full flex gap-1'>
-							<div className='whitespace-nowrap overflow-hidden text-sm max-w-fit text-ellipsis font-medium text-foreground'>Dikiy</div>
+							<div className='whitespace-nowrap overflow-hidden text-sm max-w-fit text-ellipsis font-medium text-foreground'>{notification?.from_user?.nickname || notification?.from_user?.username}</div>
 						</div>
-						<span className='text-xs leading-[1.125rem] text-white/50'>2 days ago</span>
+						<span className='text-xs leading-[1.125rem] text-white/50'>{TimePassedFromDate(notification.date)}</span>
 					</div>
 				</div>
 				<Button className='data-[subscribed=true]:!bg-[#1F1F1F] text-sm h-10 min-h-10 rounded-full'>
--- components/shared/modals/notifications/notifications.jsx
+++ components/shared/modals/notifications/notifications.jsx
@@ -9,14 +9,51 @@ import ReactionBlock from "./blocks/reaction";
 import "swiper/css";
 import "swiper/css/effect-creative";
 import FollowBlock from "./blocks/follow";
+import { useCacheFetcher } from "@/hooks/useCacheFetcher";
+import api from "@/constants/api";
+import { useStorage } from "@/hooks/contexts/session";
+import { fetcher, GetUniqueById } from "@/lib/utils";
 
 export default function NotificationsModal({ open, setOpen }) {
 	const [swiper, setSwiper] = useState(null);
 	const [active, setActive] = useState(0);
 
+	const [reactions, setReactions] = useState([]);
+	const [subscribers, setSubscribers] = useState([]);
+	const [comments, setComments] = useState([]);
+	const [pages, setPages] = useState([1, 1, 1]);
+	const { token, store } = useStorage();
+
+	const types = ["reaction", "subscribe", "comment"];
+	const url = `${api.v1}/notifications/get?page=${pages[active]}&type=${types[active]}`
+
+	const {
+		data: notificationsRequest,
+		error: notificationsError,
+		isLoading: notificationsLoading
+	} = useCacheFetcher(url, async url => await fetcher(url, "get", null, { Authorization: "Bearer " + token }));
+
+	useEffect(() => { swiper?.slideTo(active) }, [active])
+
 	useEffect(() => {
-		swiper?.slideTo(active);
-	}, [active]);
+		if (!notificationsError && !notificationsLoading) {
+			const notificationType = notificationsRequest?.success[0]?.type || types[active];
+
+			switch (notificationType) {
+				case "reaction":
+					setReactions(GetUniqueById([...reactions, ...notificationsRequest?.success]));
+					break;
+				case "subscribe":
+					setSubscribers(GetUniqueById([...subscribers, ...notificationsRequest?.success]));
+					break;
+				case "comment":
+					setComments(GetUniqueById([...comments, ...notificationsRequest?.success]));
+					break;
+				default:
+					break;
+			}
+		}
+	}, [notificationsRequest]);
 
 	return (
 		<PageModal open={open}>
@@ -43,22 +80,13 @@ export default function NotificationsModal({ open, setOpen }) {
 					modules={[EffectCreative]}
 				>
 					<SwiperSlide className='overflow-y-scroll space-y-4'>
-						<ReactionBlock />
-						<ReactionBlock />
-						<ReactionBlock />
-						<ReactionBlock />
+						{!notificationsError ? reactions.map(notification => <ReactionBlock notification={notification} />) : null}
 					</SwiperSlide>
 					<SwiperSlide className='overflow-y-scroll space-y-4'>
-						<FollowBlock />
-						<FollowBlock />
-						<FollowBlock />
-						<FollowBlock />
+						{!notificationsError ? subscribers.map(notification => <FollowBlock notification={notification} />) : null}
 					</SwiperSlide>
 					<SwiperSlide className='overflow-y-scroll space-y-4'>
-						<ReactionBlock />
-						<ReactionBlock />
-						<ReactionBlock />
-						<ReactionBlock />
+						{!notificationsError ? reactions.map(notification => <ReactionBlock notification={notification} />) : null}
 					</SwiperSlide>
 				</Swiper>
 			</div>
